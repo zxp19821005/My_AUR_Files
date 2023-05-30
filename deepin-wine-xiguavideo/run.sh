4d1d428ba43c3cc5b1c1a9a1570c21cc45f49a5e
zxp19821005 <zxp19821005@163.com>
2023-05-30 11:22:16 +0800

    update to 1.0.6

--- deepin-wine-xiguavideo/run.sh
+++ deepin-wine-xiguavideo/run.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-      
+     
 #   Copyright (C) 2016 Deepin, Inc.
 #
 #   Author:     Li LongYu <lilongyu@linuxdeepin.com>
 #               Peng Hao <penghao@linuxdeepin.com>
-      
+     
 #               Vufa <countstarlight@gmail.com>
-      
+     
 BOTTLENAME="Deepin-XiGuaVideo"
-APPVER="1.0.4spark0"
+APPVER="1.0.6"
 WINEPREFIX="${HOME}/.deepinwine/${BOTTLENAME}"
 EXEC_PATH="c:/Program Files (x86)/xigua-video-electron/西瓜视频.exe"
 EXEC_FILE="${WINEPREFIX}/drive_c/Program Files (x86)/xigua-video-electron/西瓜视频.exe"
@@ -19,42 +19,42 @@ export APPRUN_CMD="deepin-wine6-stable"
 DISABLE_ATTACH_FILE_DIALOG=""
 EXPORT_ENVS=""
 SPECIFY_SHELL_DIR=$(dirname ${START_SHELL_PATH})
-     
+    
 export SPECIFY_SHELL_DIR
-      
+     
 ARCHIVE_FILE_DIR="/opt/apps/${DEB_PACKAGE_NAME}/files"
-      
+     
 export WINEDLLPATH=/opt/${APPRUN_CMD}/lib:/opt/${APPRUN_CMD}/lib64
-      
+     
 export LD_LIBRARY_PATH=/opt/apps/${DEB_PACKAGE_NAME}/files/lib32
-      
+     
 export WINEPREDLL="${ARCHIVE_FILE_DIR}/dlls"
-      
+     
 msg() {
     ECHO_LEVEL=("\033[1;32m==> " "\033[1;31m==> ERROR: ")
     echo -e "${ECHO_LEVEL[$1]}\033[1;37m$2\033[0m"
 }
-      
+     
 OpenWinecfg() {
     msg 0 "Launching winecfg with ${APPRUN_CMD} in ${WINEPREFIX} ..."
     env WINEPREFIX="${WINEPREFIX}" ${APPRUN_CMD} winecfg
 }
-     
+    
 WakeApp() {
     env WINEPREDLL="${ARCHIVE_FILE_DIR}/dlls" \
         WINEDLLPATH=/opt/${APPRUN_CMD}/lib:/opt/${APPRUN_CMD}/lib64 \
         WINEPREFIX="${WINEPREFIX}" ${APPRUN_CMD} /opt/deepinwine/tools/sendkeys.exe w
 }
-     
+    
 Run() {
     if [ -z "${DISABLE_ATTACH_FILE_DIALOG}" ]; then
         export ATTACH_FILE_DIALOG=1
     fi
-      
+     
     if [ -n "${EXPORT_ENVS}" ]; then
         export "${EXPORT_ENVS}"
     fi
-      
+     
     if [ -n "${EXEC_PATH}" ]; then
         if [ -z "${EXEC_PATH##*.lnk}" ]; then
             msg 0 "Launching  ${EXEC_PATH} lnk file ..."
@@ -67,14 +67,14 @@ Run() {
         ${START_SHELL_PATH} ${BOTTLENAME} ${APPVER} "uninstaller.exe" "$@"
     fi
 }
-      
+     
 HelpApp() {
     echo " Extra Commands:"
     echo " winecfg          Open winecfg"
     echo " -w/--wake       Wake up background program"
     echo " -h/--help        Show program help info"
 }
-      
+     
 if [ -z "$1" ]; then
     Run "$@"
     exit 0
