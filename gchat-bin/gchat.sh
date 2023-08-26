37a5ffb0ac78fb438aca2cb8465c8d3d708b42d0
zxp19821005 <zxp19821005@163.com>
2023-08-26 12:24:13 +0800

    fix README.md

--- gchat-bin/gchat.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-_ELECTRON=/usr/bin/electron23
-_ASAR="/opt/gchat/gchat.asar"
-if [[ $EUID -ne 0 ]] || [[ $ELECTRON_RUN_AS_NODE ]]; then
-    exec ${_ELECTRON} ${_ASAR} "$@"
-else
-    exec ${_ELECTRON} ${_ASAR} --no-sandbox "$@"
-fi
