6e2c4a8af293b0e5612d1f1c66c692c7f8e53a98
Woodenman <zxp19821005@163.com>
2023-10-11 15:58:30 +0800

    fix errors

--- electron-music-player/electron-music-player.sh
+++ electron-music-player/electron-music-player.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-_ELECTRON=/usr/bin/electron24
-_ASAR=/opt/simple-music/resources/app.asar
+_ELECTRON=/usr/bin/electron25
+_ASAR="/opt/electron-music-player/resources/app.asar"
 if [[ $EUID -ne 0 ]] || [[ $ELECTRON_RUN_AS_NODE ]]; then
     exec ${_ELECTRON} ${_ASAR} "$@"
 else
