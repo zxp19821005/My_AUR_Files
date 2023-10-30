8c6241ea9bcadb41d553553105496fd9535a13a1
Woodenman <zxp19821005@163.com>
2023-10-31 05:45:55 +0800

    update to 0.19.2

--- lvce-bin/lvce.sh
+++ lvce-bin/lvce.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-_ELECTRON=/usr/bin/electron25
-_ASAR="/opt/lvce/lvce.asar"
+_ELECTRON=/usr/bin/electron27
+APPDIR="/opt/lvce"
+export PATH="${APPDIR}:${PATH}"
+_ASAR="${APPDIR}/resources/app"
 if [[ $EUID -ne 0 ]] || [[ $ELECTRON_RUN_AS_NODE ]]; then
     exec ${_ELECTRON} ${_ASAR} "$@"
 else
