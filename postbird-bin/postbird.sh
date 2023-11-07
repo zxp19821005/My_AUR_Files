be4a4fe1af307e3076fe120613c472838ff6009a
Woodenman <zxp19821005@163.com>
2023-11-07 10:41:54 +0800

    fix errors

--- postbird-bin/postbird.sh
+++ postbird-bin/postbird.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-_ELECTRON=/usr/bin/electron22
-APPDIR="/usr/lib/positron"
+_ELECTRON=/usr/bin/electron6
+APPDIR="/usr/lib/postbird"
 export PATH="${APPDIR}:${PATH}"
-#export LD_LIBRARY_PATH="${APPDIR}/swiftshader:${APPDIR}/usr/lib:${LD_LIBRARY_PATH}"
+export LD_LIBRARY_PATH="${APPDIR}/swiftshader:${LD_LIBRARY_PATH}"
 _ASAR="${APPDIR}/app.asar"
 if [[ $EUID -ne 0 ]] || [[ $ELECTRON_RUN_AS_NODE ]]; then
     exec ${_ELECTRON} ${_ASAR} "$@"
