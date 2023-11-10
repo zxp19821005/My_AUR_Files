bff4e5584ffd1c83232dcb94095b70cf821c6d01
Woodenman <zxp19821005@163.com>
2023-11-10 18:25:45 +0800

    fix errors

--- beaker-ng-bin/beaker-ng.sh
+++ beaker-ng-bin/beaker-ng.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 _ELECTRON=/usr/bin/electron13
-APPDIR=/usr/lib/anubias
+APPDIR="/usr/lib/beaker-ng"
 export PATH="${APPDIR}:${PATH}"
-export LD_LIBRARY_PATH="${APPDIR}/swiftshader:${APPDIR}/lib:${LD_LIBRARY_PATH}"
-_ASAR="${APPDIR}/app.asar"
+export LD_LIBRARY_PATH="${APPDIR}/swiftshader:${LD_LIBRARY_PATH}"
+_ASAR="${APPDIR}/app"
 if [[ $EUID -ne 0 ]] || [[ $ELECTRON_RUN_AS_NODE ]]; then
     exec ${_ELECTRON} ${_ASAR} "$@"
 else
