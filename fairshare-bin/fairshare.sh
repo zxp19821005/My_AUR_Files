922072e7fbb1e38f7d4e398e5d17d0f45b33a738
Woodenman <zxp19821005@163.com>
2023-11-14 10:59:56 +0800

    fix errors

--- fairshare-bin/fairshare.sh
+++ fairshare-bin/fairshare.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 _ELECTRON=/usr/bin/electron25
-_ASAR="/opt/fairshare/app.asar"
+APPDIR=/usr/lib/fairshare
+export PATH="${APPDIR}:${PATH}"
+export LD_LIBRARY_PATH="${APPDIR}/lib:${LD_LIBRARY_PATH}"
+_ASAR="${APPDIR}/app.asar"
+cd $APPDIR && ./api 7632
 if [[ $EUID -ne 0 ]] || [[ $ELECTRON_RUN_AS_NODE ]]; then
     exec ${_ELECTRON} ${_ASAR} "$@"
 else
