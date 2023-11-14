3febe2705ecd3393f722a84876942a22918b5f28
Woodenman <zxp19821005@163.com>
2023-11-14 17:21:54 +0800

    fix errors

--- huelectron-bin/huelectron.sh
+++ huelectron-bin/huelectron.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-_ELECTRON=/usr/bin/electron20
-APPDIR=/usr/lib/hyperkeys
+_ELECTRON=/usr/bin/electron24
+APPDIR="/usr/lib/huelectron"
 export PATH="${APPDIR}:${PATH}"
-export LD_LIBRARY_PATH="${APPDIR}/lib:${LD_LIBRARY_PATH}"
-_ASAR="${APPDIR}/app"
+#export LD_LIBRARY_PATH="${APPDIR}/swiftshader:${LD_LIBRARY_PATH}"
+_ASAR="${APPDIR}/app.asar"
 if [[ $EUID -ne 0 ]] || [[ $ELECTRON_RUN_AS_NODE ]]; then
     exec ${_ELECTRON} ${_ASAR} "$@"
 else
