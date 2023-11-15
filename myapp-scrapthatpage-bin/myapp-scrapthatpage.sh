228a018786fd61b91d38b596bfe9343128abf768
Woodenman <zxp19821005@163.com>
2023-11-15 15:57:47 +0800

    fix errors

--- myapp-scrapthatpage-bin/myapp-scrapthatpage.sh
+++ myapp-scrapthatpage-bin/myapp-scrapthatpage.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-_ELECTRON=/usr/bin/electron22
-APPDIR=/usr/lib/monit
+_ELECTRON=/usr/bin/electron23
+APPDIR="/usr/lib/myapp-scrapthatpage"
 export PATH="${APPDIR}:${PATH}"
-export LD_LIBRARY_PATH="${APPDIR}/lib:${LD_LIBRARY_PATH}"
-_ASAR="${APPDIR}/app.asar"
+#export LD_LIBRARY_PATH="${APPDIR}/swiftshader:${LD_LIBRARY_PATH}"
+_ASAR="${APPDIR}/app"
 if [[ $EUID -ne 0 ]] || [[ $ELECTRON_RUN_AS_NODE ]]; then
     exec ${_ELECTRON} ${_ASAR} "$@"
 else
