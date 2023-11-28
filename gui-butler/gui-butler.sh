afb786154c0529b434783ddc5481094cb586376a
Woodenman <zxp19821005@163.com>
2023-11-28 08:45:24 +0800

    fix errors

--- gui-butler/gui-butler.sh
+++ gui-butler/gui-butler.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 _ELECTRON=/usr/bin/electron25
-APPDIR="/usr/lib/datefile"
+APPDIR="/usr/lib/gui-butler"
 export PATH="${APPDIR}:${PATH}"
-export LD_LIBRARY_PATH="${APPDIR}/swiftshader:${LD_LIBRARY_PATH}"
-_ASAR="${APPDIR}/app.asar"
+#export LD_LIBRARY_PATH="${APPDIR}/swiftshader:${LD_LIBRARY_PATH}"
+_ASAR="${APPDIR}/app"
 if [[ $EUID -ne 0 ]] || [[ $ELECTRON_RUN_AS_NODE ]]; then
     exec ${_ELECTRON} ${_ASAR} "$@"
 else
