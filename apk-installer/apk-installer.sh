08196b5abdce71e4be2425f8aa1419b74f942d70
Woodenman <zxp19821005@163.com>
2024-01-12 15:36:48 +0800

    fix errors

--- apk-installer/apk-installer.sh
+++ apk-installer/apk-installer.sh
@@ -1,8 +1,15 @@
-#!/bin/bash
-_ELECTRON=/usr/bin/electron10
-_ASAR="/opt/apk-installer/resources/app.asar"
+#!/bin/sh
+set -e
+_APPDIR="/usr/lib/@appname@"
+_ASAR="${_APPDIR}/@appasar@"
+export PATH="${_APPDIR}:${PATH}"
+export LD_LIBRARY_PATH="${_APPDIR}/swiftshader:${_APPDIR}/lib:${LD_LIBRARY_PATH}"
+export ELECTRON_IS_DEV=0
+export NODE_ENV=production
+cd "${_APPDIR}"
 if [[ $EUID -ne 0 ]] || [[ $ELECTRON_RUN_AS_NODE ]]; then
-    exec ${_ELECTRON} ${_ASAR} "$@"
+    exec electron@electronversion@ "${_ASAR}" "$@"
 else
-    exec ${_ELECTRON} ${_ASAR} --no-sandbox "$@"
+    exec electron@electronversion@ "${_ASAR}" --no-sandbox "$@"
 fi
+exit
