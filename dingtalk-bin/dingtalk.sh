67bea5e1e3e0c65e3ed5e786e4f60229ea91ad0c
Woodenman <zxp19821005@163.com>
2026-09-17 14:00:02 +0800

    update to 8.2.8.260904001

--- dingtalk-bin/dingtalk.sh
+++ dingtalk-bin/dingtalk.sh
@@ -7,8 +7,4 @@ export QT_PLUGIN_PATH="${_APPDIR}:${QT_PLUGIN_PATH:-}"
 export QT_QPA_PLATFORM="xcb"
 export QT_AUTO_SCREEN_SCALE_FACTOR=1
 cd "${_APPDIR}" || { echo "Failed to change directory to ${_APPDIR}"; exit 1; }
-_LD_PRELOAD="${_APPDIR}/libgbm.so"
-if [ -f "${_APPDIR}/plugins/dtwebview/libcef.so" ]; then
-    _LD_PRELOAD="${_LD_PRELOAD}:${_APPDIR}/plugins/dtwebview/libcef.so"
-fi
-exec env LD_PRELOAD="${_LD_PRELOAD}" "${_RUNNAME}" "$@" >/dev/null 2>&1
+exec "${_RUNNAME}" "$@" >/dev/null 2>&1
